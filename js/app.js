1bcdb41c36b7a8a8b423df0a3b0c8d4f4ebf1e3f
Sergey P <vourtigont@saifas.org>
2020-06-19 20:51:28 +0300

    Added stats block

--- js/app.js
+++ js/app.js
@@ -433,3 +433,29 @@ function initializeMap() {
     infowindow.open(map, marker);
 }
 google.maps.event.addDomListener(window, 'load', initializeMap);
+
+
+/* ------------------------------------------------------
+ * Stat Counter
+ * ------------------------------------------------------ */
+ var statSection = $("#stats"),
+     stats = $(".stat-count");
+ statSection.waypoint({
+  handler: function(direction) {
+      if (direction === "down") {
+       stats.each(function () {
+         var $this = $(this);
+         $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (curValue) {
+              $this.text(Math.ceil(curValue));
+            }
+          });
+      });
+      }
+      // trigger once only
+      this.destroy();
+  },
+  offset: "90%"
+});
